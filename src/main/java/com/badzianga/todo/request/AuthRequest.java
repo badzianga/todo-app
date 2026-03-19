2a9cc999f69d161adaee1c8b08d2d01ba7925194
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2026-03-19 10:55:29 +0100

    Implement authentication using JWT

--- src/main/java/com/badzianga/todo/request/AuthRequest.java
+++ src/main/java/com/badzianga/todo/request/AuthRequest.java
@@ -1,9 +1,15 @@
 package com.badzianga.todo.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AuthRequest {
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a well-formed email address")
     private String email;
+    @Size(min = 3, max = 64, message = "Password must have 3-64 characters")
     private String password;
 }
