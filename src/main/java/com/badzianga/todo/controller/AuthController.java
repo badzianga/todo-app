00a9052814fa8bf7a4b5439ea283c02cdf2a540b
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2026-03-19 17:09:00 +0100

    Add exceptions for login and register

--- src/main/java/com/badzianga/todo/controller/AuthController.java
+++ src/main/java/com/badzianga/todo/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.badzianga.todo.controller;
 
+import com.badzianga.todo.exception.EmailAlreadyUsedException;
+import com.badzianga.todo.exception.InvalidEmailOrPasswordException;
 import com.badzianga.todo.request.AuthRequest;
+import com.badzianga.todo.response.ApiResponse;
 import com.badzianga.todo.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,16 +20,22 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public void register(@Valid @RequestBody AuthRequest request) {
-        System.out.println(request.getEmail());
-        System.out.println(request.getPassword());
-        authService.register(request.getEmail(), request.getPassword());
+    public ResponseEntity<ApiResponse> register(@Valid @RequestBody AuthRequest request) {
+        try {
+            authService.register(request.getEmail(), request.getPassword());
+            return ResponseEntity.ok(new ApiResponse("Success", null));
+        } catch (EmailAlreadyUsedException e) {
+            return ResponseEntity.internalServerError().body(new ApiResponse(e.getMessage(), null));
+        }
     }
 
     @PostMapping("/login")
-    public String login(@Valid @RequestBody AuthRequest request) {
-        System.out.println(request.getEmail());
-        System.out.println(request.getPassword());
-        return authService.login(request.getEmail(), request.getPassword());
+    public ResponseEntity<ApiResponse> login(@Valid @RequestBody AuthRequest request) {
+        try {
+            String token = authService.login(request.getEmail(), request.getPassword());
+            return ResponseEntity.ok(new ApiResponse("Success", token));
+        } catch (InvalidEmailOrPasswordException e) {
+            return ResponseEntity.badRequest().body(new ApiResponse(e.getMessage(), null));
+        }
     }
 }
