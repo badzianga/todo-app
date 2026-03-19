0365db9a5c19a11c94850feb4ca12879a54a22d7
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2026-03-19 10:30:14 +0100

    Implement authentication using JWT

---
+++ src/main/java/com/badzianga/todo/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.badzianga.todo.controller;
+
+import com.badzianga.todo.request.AuthRequest;
+import com.badzianga.todo.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${api.prefix}/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public void register(@RequestBody AuthRequest request) {
+        System.out.println(request.getEmail());
+        System.out.println(request.getPassword());
+        authService.register(request.getEmail(), request.getPassword());
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody AuthRequest request) {
+        System.out.println(request.getEmail());
+        System.out.println(request.getPassword());
+        return authService.login(request.getEmail(), request.getPassword());
+    }
+}
---
+++ src/main/java/com/badzianga/todo/service/JwtService.java
@@ -0,0 +1,40 @@
+package com.badzianga.todo.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private final String SECRET = "secretstringwhichwillbeextractedinthefuture";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        final Claims claims = extractAllClaims(token);
+        return resolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
