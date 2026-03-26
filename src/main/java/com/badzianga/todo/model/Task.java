0eb793a59d7187df91eaf06a174818cf6e4d6262
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2026-03-26 11:00:08 +0100

    Change strategy for entity ids to AUTO

--- src/main/java/com/badzianga/todo/model/Task.java
+++ src/main/java/com/badzianga/todo/model/Task.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Setter
 public class Task {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String description;
